36cee79f5493dc009cd05331f67f5faa147c5396
ashishjullia19 <ashishjullia19@gmail.com>
2022-11-15 13:23:54 -0500

    formatted code

--- aws-instance.tf
+++ aws-instance.tf
@@ -4,8 +4,8 @@
 # }
 
 resource "aws_instance" "instance" {
-  ami                         = data.aws_ami.ami.id
-  instance_type               = "t2.micro"
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
   # associate_public_ip_address = true
 
   vpc_security_group_ids = [aws_security_group.main.id]
